607a2c5b2060e51a7396b7ff40891fef3ffb6f50
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2024-06-28 15:53:29 +0200

    style: run prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,2 @@
-import config from "@bifravst/eslint-config-typescript";
-export default [...config, { ignores: ["dist/**", "cdk.out/**"] }];
+import config from '@bifravst/eslint-config-typescript'
+export default [...config, { ignores: ['dist/**', 'cdk.out/**'] }]
